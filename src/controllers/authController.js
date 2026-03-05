af0f40cffeeab27eab4b281835ec6772d0753142
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-03-05 00:06:01 +0000

    refactor: increase token expiry time

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -65,7 +65,7 @@ const login = async (req, res) => {
         }
 
         // Generate JWT token
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "3min" });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "10min" });
 
         // Return token
         res.json({ token });
